b61c3d7da1a57f0a77d86a5c1afc812d723754eb
Gleb Nemirko <hyfoxus@gmail.com>
2022-02-15 09:51:58 +0300

    First attempt to try, everything suprisingly works

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int firstInput = Convert.ToInt32(Console.ReadLine());
+            int secondInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Addirion: {secondInput} + {firstInput} = {secondInput + firstInput}");
+            Console.WriteLine($"Substarction: {secondInput} - {firstInput} = {secondInput - firstInput}");
+            Console.WriteLine($"Multiplication: {secondInput} * {firstInput} = {secondInput * firstInput}");
+            Console.WriteLine($"Division: {secondInput} / {firstInput} = {secondInput / firstInput}");
+            Console.WriteLine($"Remainder: {secondInput} % {firstInput} = {secondInput % firstInput}");
+
+            int[] array = new int[firstInput];
+            Random rand = new Random();
+            for (int i = 0; i < firstInput; i++)
+            {
+                array[i] = rand.Next();
+                Console.WriteLine($"Element of array {i + 1} is {array[i]}");
+            }
+        }
+    }
+}
